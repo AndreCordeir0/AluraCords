592f1e10b1978b4bc48dc9f6b69c23cd4704d850
AndreCordeir0 <andrezaocordeiro@gmail.com>
2022-01-27 13:12:38 -0300

    Enter

--- pages/chat.js
+++ pages/chat.js
@@ -1,75 +1,162 @@
-import React,{useState,useEffect} from 'react';
-import RingLoader from "react-spinners/RingLoader";
-function GlobalStyle() {
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
+    // Sua lógica vai aqui
+
+    // ./Sua lógica vai aqui
+ 
     return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-          
-        }
-      
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-      
+     
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+
+                    {/* <MessageList mensagens={[]} /> */}
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+        
     );
-  }
-    
-export default function PaginaChat(){
-<GlobalStyle/>   
-    const [loading ,setLoading] = useState(false)
+}
 
-    useEffect(()=>{
-       setLoading(true);
-        setTimeout( ()=>{
-            setLoading(false);
-        }, 4000);
-    },[]);
-    return(
+function Header() {
+    return (
+        <>
+       
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log('MessageList', props);
+    return (
     
       
-     <div className="app" 
-     >
-         <style jsx>{`
-         
-        .app{display:flex;
-        align-items:center;
-        justify-content:center;
-        height:90vh;
-        width:100%;
-        }
-         `}</style>
-         {
-         loading ?
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
 
-         <RingLoader
-         size={60}
-         color={'#123abc'}
-         loading={loading}
-         />
-         :
-<div></div>
- }
-</div>
-
-         
-    );
-    
+            <Text
+                key={mensagem.id}
+                tag="li"
+                styleSheet={{
+                    borderRadius: '5px',
+                    padding: '6px',
+                    marginBottom: '12px',
+                    hover: {
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        marginBottom: '8px',
+                    }}
+                >
+                    <Image
+                        styleSheet={{
+                            width: '20px',
+                            height: '20px',
+                            borderRadius: '50%',
+                            display: 'inline-block',
+                            marginRight: '8px',
+                        }}
+                        src={`https://github.com/vanessametonini.png`}
+                    />
+                    <Text tag="strong">
+                        {mensagem.de}
+                    </Text>
+                    <Text
+                        styleSheet={{
+                            fontSize: '10px',
+                            marginLeft: '8px',
+                            color: appConfig.theme.colors.neutrals[300],
+                        }}
+                        tag="span"
+                    >
+                        {(new Date().toLocaleDateString())}
+                    </Text>
+                </Box>
+                {mensagem.texto}
+            </Text>
+        </Box>
+      
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,39 @@ import appConfig from '../config.json';
 import {useRouter} from 'next/router'
 
 
+function Neve(){
+  return(
+<style global jsx>{`
+ body:before {
+  filter: hue-rotate(070deg);
+  position: absolute;
+  left: 0;
+  top: 0;
+  content: '';
+  width: 100%;
+  height: 100%;
+  background-image:
+url(https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAifSMTy-kzNc0gX2fMNdgI4),
+url(https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAgkQw8YVRAHWD7ejl6jGhiY);
+  animation: NEVE 90s linear infinite;
+  z-index:1 !important;
+  filter:opacity(0.2);
+  pointer-events: none !important;
+  object-fit:cover;
+}
+@keyframes NEVE {
+0% {
+    background-position: 0 0, 0 0, 0 0;
+}
+100% {
+    background-position:  -200px 400px, 300px 300px;
+}
+}
+`}</style>
 
+  )
+
+}
 function Titulo(props) {
   const Tag = props.tag || 'h1';
   return (
@@ -21,64 +53,7 @@ function Titulo(props) {
     </>
   );
 }
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-        
-        
-      }
-      
-      body:before {
-        filter: hue-rotate(070deg);
-        position: absolute;
-        left: 0;
-        top: 0;
-        content: '';
-        width: 100%;
-        height: 100%;
-        background-image:
-    url(https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAifSMTy-kzNc0gX2fMNdgI4),
-    url(https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAgkQw8YVRAHWD7ejl6jGhiY);
-        animation: NEVE 90s linear infinite;
-        z-index:1 !important;
-        filter:opacity(0.2);
-        pointer-events: none !important;
-        object-fit:cover;
-    }
-    @keyframes NEVE {
-      0% {
-          background-position: 0 0, 0 0, 0 0;
-      }
-      100% {
-          background-position:  -200px 400px, 300px 300px;
-      }
-  }
-    
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    
-  );
-}
+
   
 
 
@@ -101,13 +76,13 @@ export default function PaginaInicial() {
 const roteamento = useRouter();
   return (
     <>
-    <GlobalStyle/>
+  <Neve/>
       <Box
         styleSheet={{
             
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          background: 'url(https://i.imgur.com/inbBVfG.jpg)',filter:'brightness(0.8)',zindex:'-1',
+          background: 'url(https://i.imgur.com/inbBVfG.jpg)',filter:'brightness(0.8)',zindex:'111',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           
         }}
